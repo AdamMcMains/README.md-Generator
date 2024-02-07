35a0061ef0934b892ab3295eb54efb6026d506ed
Adam McMains <adammcmains@gmail.com>
2024-02-06 22:50:47 -0800

    add question prompts and objects

--- index.js
+++ index.js
@@ -1,49 +1,97 @@
-// TODO: Include packages needed for this application
 const package1 = require('./package.json');
 const package2 = require('./package-lock.json');
 const fs = require('fs');
 var inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-var projecttitle;
-var projectDescription;
-var questions = ["what was your motivation for making this application?","why did you build this project?","what problem does it solve?","what did you learn?"];
+const questions = [
+    {
+        type: 'input',
+        name: "Title",
+        message: "What is the Title of your Project?"
+    },
+    {
+        type: 'input',
+        name: "Motivation",
+        message: "what was your motivation for starting this project?",
+        
+    },
+    {
+        type: 'input',
+        name: "Inspiration",
+        message: "why did you build this project?",
+    },
+    {
+        type: 'input',
+        name: "Purpose",
+        message: "what problem does it solve?",
+    
+    },
+    {
+        type: 'input',
+        name: "Reflection",
+        message: "what did you learn?",
+    },
+    {
+        type: 'list',
+        name: "License",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Apache license 2.0", "MIT license", "Boost Software License 1.0", "The Unlicense"],
+    }
+];
 
-var userResponse;
+var licenseOptions = 
+    [
+        "GNU AGPLv3", 
+        "GNU GPLv3", 
+        "GNU LGPLv3", 
+        "Apache license 2.0", 
+        "MIT license", 
+        "Boost Software License 1.0", 
+        "The Unlicense"
+    ]
 
-var dataText;
+var projectTitle;
+
+var bodyText;
+
+var license;
 
 
+
+function getAnswers(){
+    return inquirer.prompt(questions).then((answers) => {
+        if (answers.License){
+            projectTitle = [answers.Title]
+            bodyText = [answers.Motivation, answers.Inspiration, answers.Purpose, answers.Reflection]
+            license = [answers.License]
+            return
+        }
+        else{
+            return getAnswers();
+        }
+    }); 
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
 
-    dataText = "Hello world."
+    dataText = data;
 
-    fs.writeFile('README.md', dataText, (err) => {
- 
-        // In case of a error throw err.
+    fs.writeFile(fileName, dataText, (err) => {
+
         if (err) throw err;
     });
 }
 
 // TODO: Create a function to initialize app
-function init() {
+function init() {}
 
-
-}
 
 // Function call to initialize app
 init();
 
-for (i = 0; i < questions.length; i++)
-inquirer.prompt([
-    {
-        name: `Answer${i}`,
-        message: questions[i],
-        type: 'input'
-    }])
-    .then(function(answer){
-        userResponse = answer;
-        console.log(userResponse);
-    });
+
+
+
+
+
+getAnswers();
