18c8b06e618d41b9bb098cbce9d7f1c1128e23fe
Adam McMains <adammcmains@gmail.com>
2024-02-07 21:38:40 -0800

    add written readme in proper order, and expanded object

---
+++ README.md
@@ -0,0 +1,35 @@
+# tex
+
+## Description
+
+text
+t
+t
+t
+## Installation
+
+t
+## Usage
+
+t
+## Credits
+
+t
+## License
+
+GNU AGPLv3
+## Badges
+
+## Features
+
+t
+## How To Contribute
+
+t
+## Tests
+
+t
+## Questions
+
+undefined
+t
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const package2 = require('./package-lock.json');
 const fs = require('fs');
 var inquirer = require('inquirer');
 
+var newLine = "\n";
+var readmeSections = ["Title","Description","Installation", "Usage", "Credits", "License", "Badges", "Features", "How To Contribute", "Tests", "Questions"];
+
 const questions = [
     {
         type: 'input',
@@ -32,38 +35,85 @@ const questions = [
         message: "what did you learn?",
     },
     {
+        type: 'input',
+        name: "Installation",
+        message: "Enter any Installation instructions if needed"
+    },
+    {
+        type: 'input',
+        name: "Usage",
+        message: "Provide instructions and examples for use"
+    },
+    {
+        type: 'input',
+        name: "Credits",
+        message: "List any collaborators if they exist, as well as any third party assets used"
+    },
+    {
+        type: 'input',
+        name: "Features",
+        message:"if your project has alot of features, list them now"
+    },
+    {
+        type: 'input',
+        name: "Contributions",
+        message: "Describe your contribution guidelines if any"
+    },
+    {
+        type: 'input',
+        name: "Tests",
+        message: "highlight any unit tests a user might be able to try"
+    },
+    {
+        type: 'input',
+        name: "Github",
+        message: "Enter your github user name so that users may reach out for questions"
+    },
+    {
+        type: 'input',
+        name: "Contact",
+        message: "please enter your email in case a user may want to contact you directly"
+    },
+    {
         type: 'list',
         name: "License",
+        message: "choose a software license that fits your project",
         choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Apache license 2.0", "MIT license", "Boost Software License 1.0", "The Unlicense"],
-    }
+    },
 ];
 
-var licenseOptions = 
-    [
-        "GNU AGPLv3", 
-        "GNU GPLv3", 
-        "GNU LGPLv3", 
-        "Apache license 2.0", 
-        "MIT license", 
-        "Boost Software License 1.0", 
-        "The Unlicense"
-    ]
-
-var projectTitle;
-
-var bodyText;
-
-var license;
-
-
-
-function getAnswers(){
+async function getAnswers(){
     return inquirer.prompt(questions).then((answers) => {
         if (answers.License){
-            projectTitle = [answers.Title]
-            bodyText = [answers.Motivation, answers.Inspiration, answers.Purpose, answers.Reflection]
-            license = [answers.License]
-            return
+            return {
+                projectTitle: `${answers.Title}`,
+
+                motivation: `${answers.Motivation}`, 
+
+                inspiration: `${answers.Inspiration}`, 
+
+                purpose: `${answers.Purpose}`, 
+
+                reflection: `${answers.Reflection}`,
+
+                installation: `${answers.Installation}`,
+
+                usage: `${answers.Usage}`,
+
+                credits: `${answers.Credits}`,
+
+                features: `${answers.Features}`,
+
+                contributions: `${answers.Contributions}`,
+
+                tests: `${answers.Tests}`,
+
+                gitHub: `${answers.Github}`,
+
+                email: `${answers.Contact}`,
+
+                license: `${answers.License}`,
+            }
         }
         else{
             return getAnswers();
@@ -71,27 +121,78 @@ function getAnswers(){
     }); 
 };
 
+
+function appendSections(file, information, sections){
+    if (sections == "Title"){
+        writeToFile(file,"# " + information.projectTitle);
+    }
+    else if(sections == "Description"){
+        addToFile(file, information.motivation)
+        addToFile(file, information.inspiration)
+        addToFile(file, information.purpose)
+        addToFile(file, information.reflection)
+    }
+    else if(sections =="Installation"){
+        addToFile(file, information.installation)
+    }
+    else if(sections =="Usage"){
+        addToFile(file, information.usage)
+    }
+    else if(sections =="Credits"){
+        addToFile(file, information.credits)
+    }
+    else if(sections =="License"){
+        addToFile(file, information.license)
+    }
+    else if(sections =="Features"){
+        addToFile(file, information.features)
+    }
+    else if(sections =="How To Contribute"){
+        addToFile(file, information.contributions)
+    }
+    else if(sections =="Tests"){
+        addToFile(file, information.tests)
+    }
+    else if(sections =="Questions"){
+        addToFile(file, information.github)
+        addToFile(file, information.email)
+    }
+    else{
+        return;
+    }
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
     dataText = data;
-
-    fs.writeFile(fileName, dataText, (err) => {
-
+    fs.writeFileSync(fileName, dataText + newLine + newLine, (err) => {
         if (err) throw err;
     });
 }
 
-// TODO: Create a function to initialize app
-function init() {}
+function addToFile(fileName, data){
+    dataText = data;
+    fs.appendFileSync(fileName, dataText +  newLine, (err) => {
+        if (err) throw err;
+    });
 
+    return;
+}
+
+// TODO: Create a function to initialize app
+async function init(file){
+    let information = await getAnswers();
+
+    for (const sections of readmeSections){ 
+
+        addToFile(file, "## " + sections  + newLine);  
+        
+        appendSections(file, information, sections);
+
+}
+}
 
 // Function call to initialize app
-init();
+init("README.md")
 
 
 
-
-
-
-getAnswers();
